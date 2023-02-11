24d6954d7289d72ceb2ea99f56df4f6c47263fb9
franklin de jesus david rodriguez <franklindavid91@gmail.com>
2023-02-10 21:33:22 -0500

    Initial commit

---
+++ CRUDNET7MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CRUDNET7MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
