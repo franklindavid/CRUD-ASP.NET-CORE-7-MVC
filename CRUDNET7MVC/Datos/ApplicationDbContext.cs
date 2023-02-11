8f57c9f931b6e0d0ca6ea19bee15c945088f6975
franklin de jesus david rodriguez <franklindavid91@gmail.com>
2023-02-11 11:27:34 -0500

    Creando  application dbcontext y el primer modelo

---
+++ CRUDNET7MVC/Datos/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CRUDNET7MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDNET7MVC.Datos
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { 
+
+        } 
+
+        public DbSet<Contacto> Contactos { get; set; }
+        
+    }
+}
